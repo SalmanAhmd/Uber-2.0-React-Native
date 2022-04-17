fb7b8eb3c019feb4d698fefa2b1e34c4815fa24b
SalmanAhmd <salmanahmed1497@gmail.com>
2022-04-18 04:08:17 +0530

    simple integration

--- App.js
+++ App.js
@@ -1,14 +1,45 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { KeyboardAvoidingView, Platform, StyleSheet, Text, View } from 'react-native';
+import { SafeAreaProvider } from 'react-native-safe-area-context';
 import { Provider } from 'react-redux';
-import HomeSceen from './screens/HomeSceen';
+import HomeScreen from './screens/HomeScreen';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
 import { store } from './store';
+import MapScreen from './screens/MapScreen';
+
+const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
     <Provider store={store}>
-      <HomeSceen />
+      <NavigationContainer>
+        <SafeAreaProvider>
+          <KeyboardAvoidingView
+            style={{ flex: 1 }}
+            behavior={Platform.OS === 'ios' ? 'padding' : 'height'}
+            keyboardVerticalOffset={Platform.OS === 'ios' ? -64 : -80}
+          >
+            <Stack.Navigator>
+              <Stack.Screen
+                name='HomeScreen'
+                component={HomeScreen}
+                options={{
+                  headerShown: false,
+
+                }} />
+              <Stack.Screen
+                name='MapScreen'
+                component={MapScreen}
+                options={{
+                  headerShown: false,
+
+                }} />
+            </Stack.Navigator>
+          </KeyboardAvoidingView>
+        </SafeAreaProvider>
+      </NavigationContainer>
     </Provider>
   );
 }
--- README.md
+++ README.md
@@ -5,3 +5,10 @@ Steps
 - expo init repo-name-here
 - setup Redux
 - 
+
+
+Directions API
+Places API
+Distance Matrix API
+
+1:44:47
